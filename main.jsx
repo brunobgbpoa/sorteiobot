967c9e924198943cf52c14fc18d30e5ef27c0c84
brunobgbpoa <bruno_borges@me.com>
2025-04-16 17:51:37 -0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Dashboard from "./painel.jsx";
+import SorteioBotPainel from "./src/SorteioBotPainel";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Dashboard />
+    <SorteioBotPainel />
   </React.StrictMode>
 );
